00d4e020d65774b6849695548a056468a1ce9f6d
Adem Kartal <Adem.Kartal@hotmail.de>
2020-07-13 16:09:20 +0200

    first version of ping_pong.py which works

--- Ch3/ping_pong.py
+++ Ch3/ping_pong.py
@@ -3,7 +3,7 @@ from mpi4py import MPI
 import numpy as np
 
 #buffer = np.array([0.0,0.0],dtype=float)
-buffer= np.arange(1000,dtype='i')
+buffer= np.array([1,2,3],dtype='f')
 #buffer = np.arange(4.)
 proc_A = 0
 proc_B = 1
@@ -15,21 +15,18 @@ size = MPI.COMM_WORLD.Get_size()
 rank = MPI.COMM_WORLD.Get_rank()
 start = MPI.Wtime()
 status = MPI.Status()
+
+
+#comm = MPI.COMM_WORLD
 for counter in range(0,number_of_messages):
     if(rank==proc_A):
-        #MPI.Comm.Send(buffer, dest= proc_B, tag=ping)
-        MPI.Comm.Send([buffer,MPI.INT], dest= proc_B, tag=ping)
-        MPI.Comm.Recv([buffer,MPI.INT],dest=proc_B,tag=pong,status=status)
-        #MPI.Comm.Recv(buf=buffer,dest=proc_B,tag=pong,status=status)
-        #MPI.Comm.Send(buffer,proc_B,ping)
-        #MPI.Comm.Recv(buffer,proc_B,pong,status)
+        MPI.COMM_WORLD.Send([buffer,MPI.FLOAT], dest= proc_B, tag=ping)
+        MPI.COMM_WORLD.Recv([buffer,MPI.FLOAT], source= proc_B, tag=pong)
+
     elif(rank==proc_B):
-        #MPI.Comm.Send(buffer=buffer,dest=proc_A,tag=pong)
-        #MPI.Comm.Recv(buffer=buffer,dest=proc_A,tag=ping)
-        MPI.Comm.Send([buffer,MPI.INT],dest=proc_A,tag=pong)
-        MPI.Comm.Recv([buffer,MPI.INT],dest=proc_A,tag=ping)
-        #MPI.Comm.Send(buffer,proc_A,pong)
-        #MPI.Comm.Recv(buffer,proc_A,ping)
+        MPI.COMM_WORLD.Send([buffer,MPI.FLOAT],dest=proc_A,tag=pong)
+        MPI.COMM_WORLD.Recv([buffer,MPI.FLOAT],source=proc_A,tag=ping)
+
 finish =MPI.Wtime()
 if(rank==proc_A):
     time = finish-start
