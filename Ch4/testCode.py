988cda97c7693b3d440fd27f5b6ac67c391010d6
Adem Kartal <Adem.Kartal@hotmail.de>
2020-08-06 18:05:54 +0200

    second comparison with ring.c

--- Ch4/testCode.py
+++ Ch4/testCode.py
@@ -25,12 +25,28 @@ sum= int()
 
 rcv_buf=my_rank
 snd_buffer=my_rank
-buffer= int()
+buffer= my_rank
+buf = int()
+buf = 0
+#print('type of my_rank: ',type(my_rank),'value of my_rank: ', my_rank)
+#buffer = my_rank
+#print('---------------------------------------------------------------------')
+#print('type of buffer: ', type(buffer), 'value of buffer: ', buffer)
 
 for counter in range(0,size):
-    req= MPI.COMM_WORLD.Send([buffer,MPI.INT], dest= right, tag=to_right)
-    print('test print')
-    ret_rcv=MPI.COMM_WORLD.Recv([buffer,MPI.INT], source= left, tag=to_right,status=status)
+
+    print('amk type of buffer: ', type(buffer), 'value of buffer: ', buffer)
+    print('---------------------------------------------------------------------')
+
+    req= MPI.COMM_WORLD.Send([buf,MPI.INT], dest= right, tag=to_right)
+    recv=MPI.COMM_WORLD.Recv([buf,MPI.INT], source= left, tag=to_right)
+    print('type of recv: ', type(recv), 'value of recv: ',recv)
+
+
+    #req= MPI.COMM_WORLD.Send([buffer,MPI.INT], dest= right, tag=to_right)
+    #req=MPI.COMM_WORLD.Recv([buffer,MPI.INT], source= left, tag=to_right)
+
+    #ret_rcv=MPI.COMM_WORLD.Recv([buffer,MPI.INT], source= left, tag=to_right,status=status)
     #ret_rcv=MPI.COMM_WORLD.Recv([rcv_buf,MPI.INT], source= left, tag=to_right,status=status)
     #print('type of ret_recv: ', type(ret_rcv), 'value of ret_rcv: ', ret_rcv)
     #snd_buffer=ret_rcv
