0a23f7cb9bcc261beb965f1b85eba3ddcfa184fb
Adem Kartal <Adem.Kartal@hotmail.de>
2020-08-09 22:04:46 +0200

    finished version of ping_pong_advanced1

--- Ch3/ping_pong_advanced1.py
+++ Ch3/ping_pong_advanced1.py
@@ -1,10 +1,6 @@
 #run with: mpiexec -n 2 python ping_pong_advanced1.py
 from mpi4py import MPI
-import numpy as np
-
-
-buffer= np.array([1,2,3],dtype='f')
-
+snd_data= 1
 proc_A = 0
 proc_B = 1
 ping=17
@@ -13,28 +9,27 @@ length_of_message=1
 number_of_messages = 50
 size = MPI.COMM_WORLD.Get_size()
 my_rank = MPI.COMM_WORLD.Get_rank()
-
 status = MPI.Status()
 
 
-if(my_rank == proc_A):
-        MPI.COMM_WORLD.Send([buffer,MPI.FLOAT], dest= proc_B, tag=ping)
-        MPI.COMM_WORLD.Recv([buffer,MPI.FLOAT], source= proc_B, tag=pong,status=status)
+if(my_rank ==proc_A):
+        MPI.COMM_WORLD.send(snd_data, dest= proc_B, tag=ping)
+        MPI.COMM_WORLD.recv(source= proc_B, tag=pong,status=status)
 
-elif(my_rank==proc_B):
-        MPI.COMM_WORLD.Recv([buffer,MPI.FLOAT], source= proc_B, tag=pong,status=status)
-        MPI.COMM_WORLD.Send([buffer,MPI.FLOAT], dest= proc_B, tag=ping)
+elif(my_rank ==proc_B):
+        MPI.COMM_WORLD.recv(source=proc_A,tag=ping,status=status)
+        MPI.COMM_WORLD.send(snd_data,dest=proc_A,tag=pong)
 
 start = MPI.Wtime()
 
 for counter in range(0,number_of_messages):
     if(my_rank==proc_A):
-        MPI.COMM_WORLD.Send([buffer,MPI.FLOAT], dest= proc_B, tag=ping)
-        MPI.COMM_WORLD.Recv([buffer,MPI.FLOAT], source= proc_B, tag=pong,status=status)
+        MPI.COMM_WORLD.send(snd_data, dest= proc_B, tag=ping)
+        MPI.COMM_WORLD.recv(source= proc_B, tag=pong,status=status)
 
     elif(my_rank==proc_B):
-        MPI.COMM_WORLD.Send([buffer,MPI.FLOAT],dest=proc_A,tag=pong)
-        MPI.COMM_WORLD.Recv([buffer,MPI.FLOAT],source=proc_A,tag=ping,status=status)
+        MPI.COMM_WORLD.recv(source=proc_A,tag=ping,status=status)
+        MPI.COMM_WORLD.send(snd_data,dest=proc_A,tag=pong)
 
 finish =MPI.Wtime()
 if(my_rank==proc_A):
