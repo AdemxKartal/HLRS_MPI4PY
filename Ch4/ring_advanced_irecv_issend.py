4ff0a7405fd2d4c14f30c0aa81d899b6e88d9f56
Adem Kartal <Adem.Kartal@hotmail.de>
2020-08-16 22:39:08 +0200

    first draft of ring advanced version

---
+++ Ch4/ring_advanced_irecv_issend.py
@@ -0,0 +1,42 @@
+from mpi4py import MPI
+#run with: mpiexec -n 2 python ring.py
+
+status = MPI.Status()
+
+request = MPI.Request()
+size = MPI.COMM_WORLD.Get_size()
+my_rank = MPI.COMM_WORLD.Get_rank()
+to_right =201
+
+
+right = (my_rank+1)%size
+left=(my_rank-1+size)%size
+# this SPMD-Sytle neighbor computation with modulo has the same meaning as:
+# right = my_rank+1
+# if(right == size):
+#   right = 0
+# left = my_rank - 1
+#if (left == -1):
+#   left = size - 1
+arr_status = [2]
+arr_request = [2]
+#status = MPI.Status(arr_status)
+sum = 1 #sum of all ranks: my_rank has value of 0, so sum=1
+snd_buf = 1
+if (my_rank==1):
+    #help(MPI.COMM_WORLD.irecv)
+    print('----------------------------------------------------')
+    #help(MPI.COMM_WORLD.isend)
+    #help(MPI.COMM_WORLD.irecv())
+    #help(MPI.Status)
+    print('-------------------------------------------')
+for counter in range(0,size-1):
+    #recv=MPI.COMM_WORLD.irecv(source=left, tag =to_right, request=arr_request[0])
+    #req=MPI.COMM_WORLD.isend(snd_buf,dest=right,tag=to_right, request=arr_request[1])
+    recv=MPI.COMM_WORLD.irecv(source=left, tag =to_right)
+    req=MPI.COMM_WORLD.isend(snd_buf,dest=right,tag=to_right)
+    req.waitall(requests=[recv])
+
+    sum = sum + counter
+
+#print('my_rank:',my_rank,'Sum=',sum)
