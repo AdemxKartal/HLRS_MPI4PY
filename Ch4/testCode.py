be4ec8cd7f6ca94b8f84e8fe2e43bbdd564701c2
Adem Kartal <Adem.Kartal@hotmail.de>
2020-08-16 16:31:08 +0200

    update code for testing

--- Ch4/testCode.py
+++ Ch4/testCode.py
@@ -4,43 +4,33 @@
 from mpi4py import MPI
 import numpy as np
 
-snd_buffer= int()
-rcv_buf=int()
-my_rank =int()
-
-
-proc_A = 0
-proc_B = 1
-ping=17
-pong=23
-length_of_message=1
-number_of_messages = 50
 size = MPI.COMM_WORLD.Get_size()
 my_rank = MPI.COMM_WORLD.Get_rank()
 
 
 right = (my_rank+1)%size
 left=(my_rank-1+size)%size
-print('value of left: ',left)
-print('value of right: ', right)
+#print('value of left: ',left)
+#print('value of right: ', right)
 status = MPI.Status()
 to_right = 201
-sum= int()
+sum= 0
 
 
-buf = 1
+buff = int()
+#buf = 1
 rcv_buf=2
-
+buffer= int()
 
 
 
 for counter in range(0,size):
-    req=MPI.COMM_WORLD.isend(buf, dest= right, tag=to_right)
-
-    MPI.COMM_WORLD.recv(rcv_buf, source= left, tag=to_right)
+    req=MPI.COMM_WORLD.isend(buff, dest= right, tag=to_right)
+    #req=MPI.COMM_WORLD.Isend([buf,MPI.INT], dest= right, tag=to_right)
+    MPI.COMM_WORLD.recv(buff, source= left, tag=to_right)
+    #MPI.COMM_WORLD.Recv([buff,MPI.INT], source= left, tag=to_right)
     req.wait()
-    buf = rcv_buf
-    sum= sum+rcv_buf
+    sum= sum+1
 
 print('sum of ranks: ', sum)
 
