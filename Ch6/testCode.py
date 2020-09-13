defcf75ee4f822fcffb1748eb4d0645b96a86d6c
Adem Kartal <Adem.Kartal@hotmail.de>
2020-09-13 23:31:52 +0200

    code for testing functions

--- Ch6/testCode.py
+++ Ch6/testCode.py
@@ -4,4 +4,6 @@ tag_ready= 7781
 request = MPI.Request()
 size = MPI.COMM_WORLD.Get_size()
 my_rank = MPI.COMM_WORLD.Get_rank()
-help(MPI.COMM_WORLD.send)
+if(my_rank==1):
+    print(help(MPI.Intracomm.scan))
+    print(type(MPI.Intracomm.scan))
