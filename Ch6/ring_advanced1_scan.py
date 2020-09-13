33c5624bd3159cee977f4fdaca61c944f2499737
Adem Kartal <Adem.Kartal@hotmail.de>
2020-09-13 23:31:23 +0200

    first draft

--- Ch6/ring_advanced1_scan.py
+++ Ch6/ring_advanced1_scan.py
@@ -6,11 +6,15 @@ tag_ready= 7781
 request = MPI.Request()
 size = MPI.COMM_WORLD.Get_size()
 my_rank = MPI.COMM_WORLD.Get_rank()
-MPI.Intracomm.Scan(sendbuf=my_rank,recvbuf=summe, op=MPI.SUM)
+token =1
+summe=0
+#scanner=MPI.Intracomm.scan(sendobj=10,op=MPI.SUM())
+MPI.Intracomm.scan(sendobj=10,op=MPI.SUM)
+
 
 if(my_rank!=0):
     MPI.COMM_WORLD.recv(source=my_rank-1,tag=tag_ready, status=status)
 print('PE: ', my_rank,' Sum: ',summe)
 
 if(my_rank!=size-1):
-    MPI.COMM_WORLD.send(obj=token,dest=proc_A,tag=pong)
+    MPI.COMM_WORLD.send(obj=token,dest=my_rank+1,tag=tag_ready)
