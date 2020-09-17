a128e13abed41e92959427350ce26920fece5dc6
Adem Kartal <Adem.Kartal@hotmail.de>
2020-09-17 14:24:12 +0200

    delete summe=1, not necessary

--- Ch6/ring_allreduce.py
+++ Ch6/ring_allreduce.py
@@ -1,4 +1,5 @@
 #mpiexec -n 2 python3 ring_allreduce.py
+#mpiexec --use-hwthread-cpus python3 ring_allreduce.py
 from mpi4py import MPI
 status = MPI.Status()
 tag_ready= 7781
@@ -6,7 +7,7 @@ request = MPI.Request()
 size = MPI.COMM_WORLD.Get_size()
 my_rank = MPI.COMM_WORLD.Get_rank()
 
-summe = 1 #default 1 because my_rank can be 0
+
 
 summe=MPI.COMM_WORLD.allreduce(sendobj=my_rank, op=MPI.SUM)
 
