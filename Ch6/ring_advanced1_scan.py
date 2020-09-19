a2b69b581b1aa78b0db9ef9ab88923eb2bde9ddf
Adem Kartal <Adem.Kartal@hotmail.de>
2020-09-19 23:03:24 +0200

    change scan function

--- Ch6/ring_advanced1_scan.py
+++ Ch6/ring_advanced1_scan.py
@@ -1,20 +1,17 @@
-#mpiexec -n 2 python ring_advanced1_scan.py
-
+#mpiexec -n 2 python3 ring_advanced1_scan.py
+#mpiexec --use-hwthread-cpus python3 ring_advanced1_scan.py
 from mpi4py import MPI
 status = MPI.Status()
 tag_ready= 7781
 request = MPI.Request()
 size = MPI.COMM_WORLD.Get_size()
 my_rank = MPI.COMM_WORLD.Get_rank()
-token =1
-summe=0
-#scanner=MPI.Intracomm.scan(sendobj=10,op=MPI.SUM())
-MPI.Intracomm.scan(sendobj=10,op=MPI.SUM)
-
-
+sum=MPI.COMM_WORLD.scan(sendobj=my_rank,op=MPI.SUM,)
+print('sum: ', sum)
+token=None
 if(my_rank!=0):
-    MPI.COMM_WORLD.recv(source=my_rank-1,tag=tag_ready, status=status)
-print('PE: ', my_rank,' Sum: ',summe)
+    token= MPI.COMM_WORLD.recv(source=my_rank-1,tag=tag_ready, status=status)
+    print('PE: ',my_rank,'Sum = ',sum)
 
 if(my_rank!=size-1):
     MPI.COMM_WORLD.send(obj=token,dest=my_rank+1,tag=tag_ready)
