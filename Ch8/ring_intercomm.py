8c4a4f62d91213c28d9c83287ec29ce5677018d1
Adem Kartal <Adem.Kartal@hotmail.de>
2020-09-23 15:06:52 +0200

    working version without comments

--- Ch8/ring_intercomm.py
+++ Ch8/ring_intercomm.py
@@ -5,13 +5,9 @@ to_right=201
 
 world_size = MPI.COMM_WORLD.Get_size()
 my_world_rank = MPI.COMM_WORLD.Get_rank()
-#status= MPI.Status()
 
 mycolor=my_world_rank > (world_size-1)/3
-#if(my_world_rank > (world_size-1)/3):
-#    mycolor=0
-#else:
- #   mycolor=1
+
 
 sub_comm= MPI.COMM_WORLD.Split(color=mycolor, key=0)
 sub_size = sub_comm.Get_size()
@@ -44,59 +40,9 @@ if(mycolor==0):
     remote_leader = 0+sub_size
 else:
     remote_leader = 0
-#inter_comm=MPI.Intracomm.Create_intercomm(self=sub_comm,local_leader=0, peer_comm=MPI.COMM_WORLD,remote_leader=remote_leader,tag=0)
-#inter_comm=sub_comm.Create_intercomm(0, MPI.COMM_WORLD,remote_leader,0)
 
-
-#Create_intercomm=sub_comm.Create_intercomm
 Create_intercomm=MPI.Intracomm.Create_intercomm
 inter_comm= MPI.Intracomm.Create_intercomm(sub_comm,0,MPI.COMM_WORLD,remote_leader,0)
-#intercomm= MPI.Intracomm.Create_intercomm(sub_comm,0,MPI.COMM_WORLD, remote_leader,0)
-#Create_intercomm=MPI.Intracomm.Create_intercomm
-
-#intercomm= Create_intercomm(sub_comm, 0,MPI.COMM_WORLD,remote_leader,0)
-#inter_comm=MPI.Intracomm.Create_intercomm(sub_comm,0, MPI.COMM_WORLD,remote_leader,0)
-#inter_comm=MPI.Intracomm.Create_intercomm()
-#print('peer_comm type: ', type(MPI.COMM_WORLD),'---','remote_leader type: ', type(remote_leader))
-#inter_comm=MPI.Intracomm.Create_intercomm(0,MPI.COMM_WORLD,remote_leader,0)
-
-
-
-    #def Create_intercomm(self,
-          #               int local_leader,
-         #                Intracomm peer_comm,
-        #                 int remote_leader,
-       #                  int tag=0):
-      #  """
-     #   Create intercommunicator
-    #    """
-      #  cdef Intercomm comm = Intercomm.__new__(Intercomm)
-     #   with nogil: CHKERR( MPI_Intercomm_create(
-    #        self.ob_mpi, local_leader,
-   #         peer_comm.ob_mpi, remote_leader,
-  #          tag, &comm.ob_mpi) )
- #       comm_set_eh(comm.ob_mpi)
-#
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 my_inter_rank = inter_comm.Get_rank()
 sumC=0
