488134482e49e2c54dacd263770789777d35b820
Adem Kartal <Adem.Kartal@hotmail.de>
2020-09-23 18:54:51 +0200

    delete not need variable

--- Ch4/ring_advanced_irecv_ssend.py
+++ Ch4/ring_advanced_irecv_ssend.py
@@ -1,9 +1,7 @@
 from mpi4py import MPI
 #run with: mpiexec -n 2 python3 ring.py
 #mpiexec --use-hwthread-cpus python3 ring_advanced_irecv_issend.py
-status = MPI.Status()
 
-request = MPI.Request()
 size = MPI.COMM_WORLD.Get_size()
 my_rank = MPI.COMM_WORLD.Get_rank()
 to_right =201
