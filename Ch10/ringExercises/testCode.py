fcb4c9b6a3ae67ecab2fa06e5a3d0c397d1dc158
Adem Kartal <Adem.Kartal@hotmail.de>
2020-10-13 16:38:45 +0200

    Makefiles for C Code

--- Ch10/ringExercises/testCode.py
+++ Ch10/ringExercises/testCode.py
@@ -1,12 +1,16 @@
 #run with: mpiexec -n 2 python3 testCode.py
 
 from mpi4py import MPI
+import numpy as np
 import inspect
-
+sizeOfInt = 4
 size = MPI.COMM_WORLD.Get_size()
 my_rank = MPI.COMM_WORLD.Get_rank()
+#rcv_buf=np.zeros(sizeOfInt,dtype='int')
+snd_buf=np.zeros(1,dtype='int')
+snd_buf[0]=10
 
 if(my_rank==0):
-    print('type snd_buf',type(snd_buf))
+    print('snd_buf: ', snd_buf,'type: ', type(snd_buf))
     #help(MPI.Win.Put)
 
