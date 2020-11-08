dff8499c69266b1dd46075f4ad5689b54281e00c
AdemxKartl <adem.kartal@hotmail.de>
2020-11-08 21:54:03 +0100

    working versioN

---
+++ Ch10/ringExercises/ring_1sided_put_win_alloc.py
@@ -0,0 +1,29 @@
+#run with: mpiexec -n 2 python3 ring_1sided_put.py
+#mpiexec --use-hwthread-cpus python3 ring_1sided_put.py
+from mpi4py import MPI
+import numpy as np
+size = MPI.COMM_WORLD.Get_size()
+my_rank = MPI.COMM_WORLD.Get_rank()
+to_right =201
+sizeOfInt = 4
+sizeOfMemory = np.zeros(sizeOfInt, dtype='int')
+
+right = (my_rank+1)%size
+left=(my_rank-1+size)%size
+memory=MPI.Alloc_mem(sizeOfInt,MPI.INFO_NULL)
+rcv_buf=np.zeros(1,dtype='int') # is that okay? window size just one
+snd_buf=np.zeros(1,dtype='int')
+
+win=MPI.Win.Create(memory,disp_unit=sizeOfInt, comm=MPI.COMM_WORLD)
+
+sum = 0
+snd_buf[0]=my_rank
+
+for counter in range(0,size):
+    win.Fence(MPI.MODE_NOSTORE|MPI.MODE_NOPRECEDE)
+    win.Put(snd_buf,right,None)
+    win.Fence(MPI.MODE_NOSTORE|MPI.MODE_NOPUT|MPI.MODE_NOSUCCEED)
+    snd_buf[0]=rcv_buf[0]
+    sum= sum+rcv_buf[0]
+
+print('my_rank:',my_rank,'Sum=',sum)
