5efb8013d924762930244ac18f91d48e421e02af
AdemxKartl <adem.kartal@hotmail.de>
2020-12-22 11:50:27 +0100

    first draft

---
+++ Ch13/mpi_io_exa1.py
@@ -0,0 +1,16 @@
+#run with: mpiexec -n 2 python3 mpi_io_exa1.py
+
+from mpi4py import MPI
+
+size = MPI.COMM_WORLD.Get_size()
+my_rank = MPI.COMM_WORLD.Get_rank()
+status=MPI.Status()
+fh = MPI.File.Open(comm=MPI.COMM_WORLD, filename='my_test_file', amode= MPI.MODE_RDWR| MPI.MODE_CREATE, info = MPI.INFO_NULL)
+
+for counter in range (0,10):
+    buf = '0' + str(my_rank)
+    offset= my_rank+size*counter
+    fh.Write_at(offset=offset, buf=buf, status=status)
+    #MPI.File.Write_at(offset=offset,buf=buf,status=status)
+fh.Close()
+print(' PE: ', my_rank)
