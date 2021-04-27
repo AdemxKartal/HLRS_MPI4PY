4169632b410c33d812e29eb709ae3bc345b107e6
st150018 <st150018@stud.uni-stuttgart.de>
2021-04-27 13:50:39 +0200

    change tag value

--- Ch4/ring-WRONG1.py
+++ Ch4/ring-WRONG1.py
@@ -5,7 +5,7 @@ from mpi4py import MPI
 
 size = MPI.COMM_WORLD.Get_size()
 my_rank = MPI.COMM_WORLD.Get_rank()
-to_right =201
+
 
 
 right = (my_rank+1)%size
@@ -22,9 +22,9 @@ sum = 0
 snd_buf = my_rank
 for counter in range(0,size):
     status = MPI.Status()
-    req=MPI.COMM_WORLD.send(obj=snd_buf,dest=right,tag=to_right)
+    req=MPI.COMM_WORLD.send(obj=snd_buf,dest=right,tag=17)
     #WRONG programm, because if MPI_Send is implemented with a synchronous communication protocol then this programm will be deadlock!!!
-    recv_buf=MPI.COMM_WORLD.recv(source=left, tag =to_right, status=status)
+    recv_buf=MPI.COMM_WORLD.recv(source=left, tag =17, status=status)
     sum = sum + snd_buf
 
 
