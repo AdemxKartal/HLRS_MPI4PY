d5bef57b0357f742c76d6c6a5d08f685bae61aed
st150018 <st150018@stud.uni-stuttgart.de>
2021-04-27 12:01:27 +0200

    start Ch12

--- Ch12/derived_11.py
+++ Ch12/derived_11.py
@@ -1,11 +1,25 @@
 #run with: mpiexec -n 2 python3 ring.py
 #mpiexec --use-hwthread-cpus python3 ring.py
 
-from mpi4py import MPI
 
+
+from mpi4py import MPI
+to_right = 201
+COUNT = 2
 size = MPI.COMM_WORLD.Get_size()
 my_rank = MPI.COMM_WORLD.Get_rank()
-to_right =201
 
 
+status = MPI.Status()
+request = MPI.Request()
+right = (my_rank+1)%size
+left=(my_rank-1+size)%size
 
+array_of_blocklengths= [COUNT]
+
+array_of_blocklengths[0]=1
+array_of_blocklengths[1]=1
+
+int_snd_buf = my_rank
+
+MPI.Get_adress(int_snd_buf)
