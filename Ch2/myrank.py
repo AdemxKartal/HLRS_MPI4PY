6c293f71bbadf386cf30c7c724873e81b1b0303b
st150018 <st150018@stud.uni-stuttgart.de>
2021-04-27 12:57:48 +0200

    rename file

--- Ch2/myrank.py
+++ Ch2/myrank.py
@@ -1,5 +1,5 @@
 from mpi4py import MPI
-#run with: mpiexec -n 2 python helloMPI_advanced.py
+#run with: mpiexec -n 2 python myrank.py
 
 rank = MPI.COMM_WORLD.Get_rank()
 print("Hello World! I am process: ",rank)
--- Ch3/ping_pong.py
+++ Ch3/ping_pong.py
@@ -17,13 +17,15 @@ status = MPI.Status()
 
 for counter in range(0,number_of_messages):
     if(my_rank ==proc_A):
+        print("I am ", my_rank, "before sending ping")
         MPI.COMM_WORLD.send(buffer, dest= proc_B, tag=ping)
         MPI.COMM_WORLD.recv(source= proc_B, tag=pong,status=status)
-
+        print("I am ", my_rank, "after recv pong")
     elif(my_rank ==proc_B):
+        print("I am ", my_rank, "before sending ping")
         MPI.COMM_WORLD.recv(source=proc_A,tag=ping,status=status)
         MPI.COMM_WORLD.send(buffer,dest=proc_A,tag=pong)
-
+        print("I am ", my_rank, "after recv pong")
 finish =MPI.Wtime()
 
 if(my_rank ==proc_A):
